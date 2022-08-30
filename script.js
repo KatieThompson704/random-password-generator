086acaf18cd701fcb71892e376b6dd4e3a0197d4
Katie <thompsonkatherine704@gmail.com>
2022-08-30 19:51:55 -0400

    created global variables and added confirm prompts in js

--- script.js
+++ script.js
@@ -1,53 +1,159 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numericCharArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialCharArray = [
+  " ",
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "()",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+
+// 1. Prompt the user for the password criteria
+// 1a. User PROMPT Password Length between 8 < 128
+// 1b. User CONFIRMS: lowercase, uppercase, numbers, special characters
+
+function promptUser() {
+  var passwordCriteria1 = prompt(
+    "How many characters do you want your password to be? (Minimum 8 and Max 128 required)"
+  );
+  var passwordLength = Number(passwordCriteria1);
+  var passwordCriteria2 = confirm(
+    "Character Type 1: Do you want lowercase letters in your password?"
+  );
+  var passwordCriteria3 = confirm(
+    "Character Type 2: Do you want UPPERCASE letters in your password?"
+  );
+  var passwordCriteria4 = confirm(
+    "Character Type 3: Do you want numeric characters in your password?"
+  );
+  var passwordCriteria5 = confirm(
+    "Character Type 4: Do you want special characters in your password?"
+  );
+  if (
+    7 < passwordLength < 129 &&
+    passwordCriteria2 &&
+    passwordCriteria3 &&
+    passwordCriteria4 &&
+    passwordCriteria5
+  ) {
+    console.log("maximum security password!");
+  } else {
+    console.log("password securtiy criteria not met, please try again!");
+  }
+  var responses = {
+    length: passwordLength,
+    lowercase: passwordCriteria2,
+    uppercase: passwordCriteria3,
+    numeric: passwordCriteria4,
+    special: passwordCriteria5,
+  };
+  return responses;
+}
+
+// 2. Validate the input .
+// 3. Generate password based on criteria
+// 4. Display the generated password on the page
+
+function generatePassword() {
+  var userInput = promptUser();
+  console.log(test);
+}
+
+// for (i= 8; <=passwordCriteria1; ++){
+//   console.log("This is the current value of passwordCriteria1: " + passwordCriteria1 +".");
+// }
 
 // Write password to the #password input
 function writePassword() {
-    let text;
-    let passwordCriteria1 = prompt ("How many characters do you want your password to be? (Minimum 8 and Max 128 required)", "8");
-    let passwordCriteria2 = prompt ("Character Type 1: Do you want lowercase letters in your password? (Yes or No)", "yes");
-    let passwordCriteria3 = prompt ("Character Type 2: Do you want UPPERCASE letters in your password? (Yes or No)", "yes");
-    let passwordCriteria4 = prompt ("Character Type 3: Do you want numeric characters in your password? (Yes or No)", "yes");
-    let passwordCriteria5 = prompt ("Character Type 4: Do you want special characters in your password? (Yes or No)", "yes");
-}
-
-// function generatePassword(){
-//     var passwordLength = passwordCriteria1;
-//     if passwordCriteria2 = "yes" {
-
-//     }
-//     else if passwordCriteria2 = "no"{
-
-//     };
-//     if passwordCriteria3 = "yes" {
-
-//     }
-//     else if passwordCriteria3 = "no"{
-        
-//     };
-//     if passwordCriteria4 = "yes" {
-
-//     }
-//     else if passwordCriteria4 = "no"{
-        
-//     };
-//     if passwordCriteria5 = "yes" {
-
-//     }
-//     else if passwordCriteria5 = "no"{
-        
-//     };
-// }
-
-
-  var password = generatePassword()
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  alert("Your password is " + password);
-
-
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
